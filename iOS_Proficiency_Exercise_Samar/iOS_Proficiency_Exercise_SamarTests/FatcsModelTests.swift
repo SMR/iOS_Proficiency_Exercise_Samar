8c44d5cdb1fd2f1b80e8c87066e6b47489e10578
Samarjeet <isparshit@gmail.com>
2020-09-26 23:48:16 +0530

    Facts Model Unit Test added  tested  with  Sample Data and URL

--- iOS_Proficiency_Exercise_Samar/iOS_Proficiency_Exercise_SamarTests/FatcsModelTests.swift
+++ iOS_Proficiency_Exercise_Samar/iOS_Proficiency_Exercise_SamarTests/FatcsModelTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-
+@testable import iOS_Proficiency_Exercise_Samar
 class FatcsModelTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -28,6 +28,38 @@ class FatcsModelTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    func testFactsModelStructure() throws {
+      let noFactsModel = FactsModel(title: "About Canada", rows: [])
+
+      XCTAssertNotNil(noFactsModel)
+      XCTAssertEqual(noFactsModel.title, "About Canada")
+      XCTAssertNotNil(noFactsModel.rows)
+      XCTAssertEqual(noFactsModel.rows!.count, 0)
+
+      let sampleTitle = "Beavers"
+      let sampleDescription = """
+                              Beavers are second only to humans in their ability
+                              to manipulate and change their environment. They can measure up to
+                              1.3 metres long. A group of beavers is called a colony.
+                              """
+      let sampleImageURL = """
+                            https://upload.wikimedia.org/wikipedia/commons/thumb/6
+                            /6b/American_Beaver.jpg/220px-American_Beaver.jpg
+                            """
+
+      let factsModel = FactsModel(
+        title: "About Canada",
+        rows: [
+          Row(title: sampleTitle, rowDescription: sampleDescription, imageHref: sampleImageURL)
+        ]
+      )
+
+      XCTAssertNotNil(factsModel)
+      XCTAssertEqual(factsModel.rows!.count, 1)
+      XCTAssertEqual(factsModel.rows![0].title, sampleTitle)
+      XCTAssertEqual(factsModel.rows![0].rowDescription, sampleDescription)
+      XCTAssertEqual(factsModel.rows![0].imageHref, sampleImageURL)
     }
 
 }
