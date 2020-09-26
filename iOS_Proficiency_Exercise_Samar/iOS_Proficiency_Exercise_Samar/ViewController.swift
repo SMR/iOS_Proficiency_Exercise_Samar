ad15dd46c6f74217a6812ed8c9b14c21d6a8a0da
Samarjeet <isparshit@gmail.com>
2020-09-26 19:31:50 +0530

    1.Initial project setup 2.Folder structure created for MVVM Architecture

---
+++ iOS_Proficiency_Exercise_Samar/iOS_Proficiency_Exercise_Samar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Proficiency_Exercise_Samar
+//
+//  Created by iSparshiT on 26/09/20.
+//  Copyright © 2020 Samarjeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
